d4737e8e3d3a23a8c959b6bff29b1bff2bc6cc6f
ShubhamGlassfrog <110092916+ShubhamGlassfrog@users.noreply.github.com>
2024-05-08 12:57:34 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,16 +13,7 @@ export default function RootLayout({ children }) {
   return (
     
     <html lang="en">
-    <Head>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-      </Head>
+   
       <body className={inter.className}>{children}</body>
     </html>
   );
